64bce5c741eeac2ab0b68f9fe4359e995f1ca69e
Danstan Oduor <danstan.oduor@student.moringaschool.com>
2022-08-28 21:57:47 +0300

    fetch lab

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => console.log(function(){
+    renderBooks
+    }));
+    return fetchBooks();
+
 }
 
 function renderBooks(books) {
