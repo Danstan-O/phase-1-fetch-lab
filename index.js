2146a0cf285c9de4c5d2b67c36d6e637fd2dbdbc
danstan <danstan.oduor@student.moringaschool.com>
2022-09-03 19:15:02 +0300

    fetch lab

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  fetch("https://anapioficeandfire.com/api/books")
-  .then((resp) => resp.json())
-  .then((json) => console.log(function(){
-    renderBooks
-    }));
-    return fetchBooks();
-
-}
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+ function fetchBooks() {
+    // To pass the tests, don't forget to return your fetch!
+    return fetch('https://anapioficeandfire.com/api/books')
+    .then(function(response){
+      return response.json()
+    }).then(function(data){renderBooks(data)})
+    
+  }
+  
+  function renderBooks(books) {
+    const main = document.querySelector('main');
+    books.forEach(book => {
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchBooks();
   });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
